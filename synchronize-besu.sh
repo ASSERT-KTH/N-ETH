92becc48bfa46cc04568ad60243ff27219a15458
Javier Ron Arteaga <javierron90@gmail.com>
2022-11-10 11:59:45 +0100

    split disk setup
    Signed-off-by: Javier Ron Arteaga <javierron90@gmail.com>

--- synchronize-besu.sh
+++ synchronize-besu.sh
@@ -1,41 +1,5 @@
 #!/bin/bash
 
-# ssd setup
-
-# find device name
-echo "Finding snapshot partition name..."
-SSD_PARTITION=`lsblk | grep 1.5T | awk -e '$0 ~ /sd.\s/ {print $1}'`
-SSD_PARTITION=/dev/"$SSD_PARTITION"1
-echo "Partition name found : $SSD_PARTITION"
-
-# mount
-SSD_MOUNT_POINT=/home/javier/ssd
-sudo mount $SSD_PARTITION $SSD_MOUNT_POINT
-
-
-# nvme setup
-# nvme create partition
-NVME_DEVICE=/dev/nvme0n1
-sudo fdisk $NVME_DEVICE <<EOF
-n
-p
-1
-
-
-w
-EOF
-
-# format new partition
-NVME_PARTITION=/dev/nvme0n1p1
-sudo mkfs.ext4 -F $NVME_PARTITION
-
-# copy eth state from snapshot ? maybe copy directory instead of partition
-sudo dd if=$SSD_PARTITION of=$NVME_PARTITION bs=500M status=progress
-
-# mount nvme
-NVME_MOUNT_POINT=/home/javier/nvme
-sudo mount $NVME_PARTITION $NVME_MOUNT_POINT
-
 # start besu
 BESU_DIR=/home/javier/besu/build/install/besu/bin
 BESU_LOG=/home/javier/besu-sync-"$(date -I)".log
@@ -113,7 +77,7 @@ done
 echo "Sync complete!"
 # copy eth state to snapshot
 
-# rsync --delete -r nvme/ ssd
+rsync --delete -r nvme/ ssd
 
 # # save snapshot
 
--- synchronize-erigon.sh
+++ synchronize-erigon.sh
@@ -1,41 +1,5 @@
 #!/bin/bash
 
-# ssd setup
-
-# find device name
-echo "Finding snapshot partition name..."
-SSD_PARTITION=`lsblk | grep 1.5T | awk -e '$0 ~ /sd.\s/ {print $1}'`
-SSD_PARTITION=/dev/"$SSD_PARTITION"1
-echo "Partition name found : $SSD_PARTITION"
-
-# mount
-SSD_MOUNT_POINT=/home/javier/ssd
-sudo mount $SSD_PARTITION $SSD_MOUNT_POINT
-
-
-# nvme setup
-# nvme create partition
-NVME_DEVICE=/dev/nvme0n1
-sudo fdisk $NVME_DEVICE <<EOF
-n
-p
-1
-
-
-w
-EOF
-
-# format new partition
-NVME_PARTITION=/dev/nvme0n1p1
-sudo mkfs.ext4 -F $NVME_PARTITION
-
-# copy eth state from snapshot ? maybe copy directory instead of partition
-sudo dd if=$SSD_PARTITION of=$NVME_PARTITION bs=500M status=progress
-
-# mount nvme
-NVME_MOUNT_POINT=/home/javier/nvme
-sudo mount $NVME_PARTITION $NVME_MOUNT_POINT
-
 # start erigon
 ERIGON_DIR=/home/javier/erigon/build/bin
 ERIGON_LOG=/home/javier/erigon-sync-"$(date -I)".log
@@ -105,7 +69,7 @@ echo "Sync complete!"
 
 # copy eth state to snapshot
 
-# rsync --delete -r nvme/ ssd
+rsync --delete -r nvme/ ssd
 
 # # save snapshot
 
