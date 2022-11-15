6645b0b599b0b32a5cd335527781eaaa1ee07300
Javier Ron Arteaga <javierron90@gmail.com>
2022-11-15 23:23:16 +0100

    request test
    Signed-off-by: Javier Ron Arteaga <javierron90@gmail.com>

--- requests.go
+++ requests.go
@@ -8,16 +8,23 @@ import (
 	"net/http"
 )
 
+type Request struct {
+	Method  string        `json:"method"`
+	Params  []interface{} `json:"params"`
+	Id      int           `json:"id"`
+	Jsonrpc string        `json:"jsonrpc"`
+}
+
 func main() {
 
 	// {"method":"eth_getBlockByNumber","params":["0xa55e27", false],"id":1,"jsonrpc":"2.0"}
-	values := map[string]string{
-		"method":  "eth_getBlockByNumber",
-		"params":  "[\"0xa55e27\", false]",
-		"id":      "1",
-		"jsonrpc": "2.0",
+	request := Request{
+		Method:  "eth_getBlockByNumber",
+		Params:  []interface{}{"0xa55e27", false},
+		Id:      1,
+		Jsonrpc: "2.0",
 	}
-	json_data, err := json.Marshal(values)
+	json_data, err := json.Marshal(request)
 
 	resp, err := http.Post("http://localhost:4545", "application/json", bytes.NewBuffer(json_data))
 	if err != nil {
